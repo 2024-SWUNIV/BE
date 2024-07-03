1e9815f16531daa52dcd723bcfe9eb8253d39c78
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-07-04 01:01:42 +0900

    chore: 도메인형 디렉토리 구조를 구성한다. (#1) * setting: 프로젝트 초기 세팅을 한다.

---
+++ src/test/java/shapy/shapy/ShapyApplicationTests.java
@@ -0,0 +1,13 @@
+package shapy.shapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
